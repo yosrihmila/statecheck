aae601fede1d4e0cb0a280f5cfb0bf1abab7e342
yosrihmila <yyosri433@gmail.com>
2024-01-28 10:37:56 +0100

    newcommit'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state ={
+    Person:{
+      fullName: "Yosri Hmila",
+      bio: "React.js student",
+      imgSrc : "https://media.architecturaldigest.com/photos/63079fc7b4858efb76814bd2/16:9/w_4000,h_2250,c_limit/9.%20DeLorean-Alpha-5%20%5BDeLorean%5D.jpg" ,
+      profession : "Student"
+    },
+    show: false
+  }
+  showContent =()=> {
+    this.setState({show:!this.state.show})
+  }
+  render() {
+    return (
+      <div> <div>
+          {this.state.show?<><h1>{this.state.Person.fullName}</h1>
+          <h2>{this.state.Person.bio}</h2>
+          <img width={"150px"} src={this.state.Person.imgSrc}></img>
+          </>:null}
+          </div>
+          <div><button onClick={this.showContent}>Show</button>
+          </div>
+          </div>
+    )
+  }
 }
-
-export default App;
